5129c1f9f14610ca30fabee4a8ae53112bdde710
jslozowski <93130978+jslozowski@users.noreply.github.com>
2022-01-28 13:32:04 +0100

    Update test_funkcja.py

--- test/test_funkcja.py
+++ test/test_funkcja.py
@@ -7,11 +7,7 @@ def test_potega_dodatnia():
     assert funkcja.potega_dodatnia(2,0) == 1
     assert funkcja.potega_dodatnia(5,-1) == "b ma byc dodatnie"
 
-test_potega_dodatnia()
-
 def test_dodawanie_dwoch():
     assert funkcja.dodawanie_dwoch(2,2) == 4
     assert funkcja.dodawanie_dwoch(2,0) == 2
     assert funkcja.dodawanie_dwoch(2,-1) == 1
-
-test_dodawanie_dwoch()
